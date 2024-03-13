038f3ef8391b7927177b25000f12504f2729a04d
gieeun <gieun0528@gamil.com>
2024-03-13 16:24:58 +0900

    static 선언

--- seshop/src/seshop/com/sds/seshop/lib/FileManager.java
+++ seshop/src/seshop/com/sds/seshop/lib/FileManager.java
@@ -3,7 +3,7 @@ package seshop.com.sds.seshop.lib;
 //파일과 관련한 유요한 기능들을 지원하는 나만의 클래스
 public class FileManager {
 	
-	public String getExt(String filename) {	
+	public static String getExt(String filename) {	
 		//선택한 파일에서 "."까지 이전 문자열은 지우고 반환
 		String ext =filename.substring(filename.lastIndexOf(".")+1,filename.length() ); //확장자
 		System.out.println("확장자는 "+ext);
