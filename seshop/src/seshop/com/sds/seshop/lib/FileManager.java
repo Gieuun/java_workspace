5d45d97bc9d4aa70ee244fa37df73278a7006e93
gieeun <gieun0528@gamil.com>
2024-03-13 16:24:33 +0900

    선택한 파일을 확장자를 반환해주는 메서드

---
+++ seshop/src/seshop/com/sds/seshop/lib/FileManager.java
@@ -0,0 +1,14 @@
+package seshop.com.sds.seshop.lib;
+
+//파일과 관련한 유요한 기능들을 지원하는 나만의 클래스
+public class FileManager {
+	
+	public String getExt(String filename) {	
+		//선택한 파일에서 "."까지 이전 문자열은 지우고 반환
+		String ext =filename.substring(filename.lastIndexOf(".")+1,filename.length() ); //확장자
+		System.out.println("확장자는 "+ext);
+		
+		return ext; //확장자 명 반환
+	}
+
+}
