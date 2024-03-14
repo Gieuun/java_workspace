157b2cfad69101d37df1cda6f8ecffc26084b3f7
gieeun <gieun0528@gamil.com>
2024-03-14 16:08:29 +0900

    웹에서 정적 자원 크롤링 구현

--- seshop/src/seshop/com/sds/seshop/product/ProductRegist.java
+++ seshop/src/seshop/com/sds/seshop/product/ProductRegist.java
@@ -2,8 +2,17 @@ package seshop.com.sds.seshop.product;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,10 +21,12 @@ import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.JTextField;
 
+import seshop.com.sds.seshop.lib.FileManager;
 import seshop.com.sds.seshop.main.Page;
 import seshop.com.sds.seshop.main.ShopMain;
 
@@ -56,7 +67,7 @@ public class ProductRegist extends Page {
 		b_sub = new JComboBox();
 
 		t_product_name = new JTextField();
-		t_price = new JTextField();
+		t_price = new JTextField("숫자만 넣으세요");
 		t_brand = new JTextField();
 		t_url = new JTextField();
 
@@ -132,7 +143,7 @@ public class ProductRegist extends Page {
 					// 대입이 가능한 현상을 unBoxing 이라 한다..이것 또한 편의성때문에 지원한다..
 
 					if (index > 0) { // 0번째는 이미 안내 문구가 들어있으므로, 0번째 보다 큰 애들만 반응하자
-						int topcategory_idx = topIdxList.get(index-1); //안내문구로 늘어난 인덱스 수 맞추기
+						int topcategory_idx = topIdxList.get(index - 1); // 안내문구로 늘어난 인덱스 수 맞추기
 						System.out.println("부모의 주민번호는 " + topcategory_idx);
 
 						getSubCategory(topcategory_idx); // 서브 카테고리 목록 가져오기
@@ -140,6 +151,72 @@ public class ProductRegist extends Page {
 				}
 			}
 		});
+
+		// 수집 버튼에 리스너 연결
+		bt_collect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				downLoadFromURL();
+
+			}
+		});
+	}
+
+	// 인터넷상의 이미지 주소를 이용하여, 나의 하드디스크에 그 이미지를 저장하자(수집)
+	// 웹서버로부터 정적 자원을 가져오려면, HTTP 프로토콜을 사용해야 한다..
+	// javaSE에서는 HttpURLConnection 객체가 웹상의 요청을 시도할 수 있는 객체로 지원됨.
+	// 즉 웹브라우저의 요청 행위를 따라해보자..이 실습을 위해 오전의 톰켓의 이미지를 다운로드 해보자
+	public void downLoadFromURL() {
+		URLConnection urlCon = null;
+		HttpURLConnection httpCon = null; // 웹 상의 요청을 시도하고 응답을 가져올 수 있는 객체
+											// 즉 웹브라우저의 역할을 일 부 수행할 수 있는 객체
+		InputStream is = null; // finally에서 닫아야 하므로 try문 밖에 선언
+		FileOutputStream fos = null; // 파일을 대상으로 한 바이트 기반출력 스트림
+
+		try {
+			URL url = new URL(t_url.getText());
+			urlCon = url.openConnection(); // 이 시점에 고양이가 반응을 할까?
+			httpCon = (HttpURLConnection) urlCon; // 부모자료형에서 자식자료형으로 down Casting
+
+			// 웹서버에 있는 정적 자원에 대해 스트림을 꽂아버리자!!!
+			is = httpCon.getInputStream();
+
+			// 읽어들인 데이터를 출력시킬 파일 출력스트림 생성
+			long time = System.currentTimeMillis();
+			String ext = FileManager.getExt(t_url.getText());
+			String myName = time+"."+ext;
+			
+			fos = new FileOutputStream("C:/Users/gieun/SeShop/"+myName);
+
+			int data = -1;
+
+			while (true) {
+				data = is.read(); // 스트림으로부터 1 byte 읽어오기
+				if (data == -1)
+					break; // 파일의 끝을 만나면 루프 중단
+				System.out.println(data);
+				fos.write(data); //1byte 내려쓰기
+			}
+			JOptionPane.showMessageDialog(this, "이미지 수집완료");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			if(fos != null) {
+				try {
+					fos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}if(is !=null) {
+			try {
+				is.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
 	}
 
 	public void getTopCategory() {
